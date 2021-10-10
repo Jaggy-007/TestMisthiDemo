5babe33c0a3b8d7faea87b18d2b821c667a45c41
ashis <ashis@LAPTOP-myfiosgateway.com>
2021-10-09 20:17:54 -0400

    First Test Project Commit -001

---
+++ com.packeage.dept/src/main/java/com/packeage/company/com/packeage/dept/App.java
@@ -0,0 +1,13 @@
+package com.packeage.company.com.packeage.dept;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
